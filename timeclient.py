c72b176b729f4d6138168e053154755c8dbe4420
ArimaBatista <80778627+ArimaBatista@users.noreply.github.com>
2024-06-28 16:38:27 -0300

    Update timeclient.py

--- timeclient.py
+++ timeclient.py
@@ -1,15 +1,17 @@
 # Cliente (client.py)
 import xmlrpc.client
 import time
+import threading
+
 proxy = xmlrpc.client.ServerProxy("http://192.168.0.115:8000/")
 
 
 # Função que incrementa o tempo lógico periodicamente
 def increment_local_time():
-    global local_time
-    while True:
-        time.sleep(1)  # Espera 1 segundo
-        local_time += 1
+	global local_time
+	while True:
+    	time.sleep(1)  # Espera 1 segundo
+    	local_time += 1
 
 local_time = 0  # Inicialização do tempo lógico local
 
@@ -18,9 +20,10 @@ increment_thread = threading.Thread(target=increment_local_time)
 increment_thread.daemon = True
 increment_thread.start()
 # Simulação de eventos
-while(0 == 0)
-  imput("evento")
+while(0 == 0):
+  t = input("evento")
   print("Tempo lógico após evento local:", local_time)
   remote_time = proxy.update_time(local_time)  # Envio de mensagem
   local_time = max(local_time, remote_time) + 1
   print("Tempo lógico após envio de mensagem:", local_time)
+
