daacf361af0c407188e000cc04db675478a6d128
ArimaBatista <80778627+ArimaBatista@users.noreply.github.com>
2024-06-28 15:32:43 -0300

    Update timeserve.py

--- timeserve.py
+++ timeserve.py
@@ -1,12 +1,34 @@
 from xmlrpc.server import SimpleXMLRPCServer
+import time
 
 def update_time(remote_time):
-    global local_time
-    local_time = max(local_time, remote_time) + 1
-    return local_time
+  """
+  Atualiza o `local_time` com o valor máximo entre o `remote_time` e o `local_time` atual, 
+  adicionando 1.
+  """
+  global local_time
+  local_time = max(local_time, remote_time) + 1
+  return local_time
 
-local_time = 0  # Inicialização do tempo lógico local
+local_time = 0 # Inicialização do tempo lógico local
 
-server = SimpleXMLRPCServer(("localhost", 8000))
+server = SimpleXMLRPCServer(("192.168.0.115", 8000))
 server.register_function(update_time, "update_time")
-server.serve_forever()
+
+def increment_local_time():
+  """
+  Incrementa o `local_time` a cada segundo.
+  """
+  global local_time
+  local_time += 1
+
+# Inicia um timer para incrementar o `local_time` a cada segundo
+timer = time.time()
+while True:
+  # Verifica se o timer já passou de um segundo
+  if time.time() - timer > 1:
+    increment_local_time()
+    timer = time.time()
+
+  # Processa as requisições RPC
+  server.handle_request()
