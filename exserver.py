db8938473e8d03b4ee5824215cef33f67aaadad6
ArimaBatista <80778627+ArimaBatista@users.noreply.github.com>
2024-06-30 17:12:08 -0300

    Update exserver.py

--- exserver.py
+++ exserver.py
@@ -24,7 +24,6 @@ def liberar_recurso(client_id):
         return False
 
 server = SimpleXMLRPCServer(("192.168.0.115", 8000))
-print("Servidor XML-RPC em execução...")
 
 server.register_function(solicitar_recurso, "solicitar_recurso")
 server.register_function(liberar_recurso, "liberar_recurso")
