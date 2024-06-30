9172cb414bfbb1c023954556578e9a3beac6bff1
ArimaBatista <80778627+ArimaBatista@users.noreply.github.com>
2024-06-30 17:11:28 -0300

    Update exserver.py

--- exserver.py
+++ exserver.py
@@ -4,6 +4,8 @@ flag = False  # Indica se o recurso está em uso
 client = None  # ID do cliente que possui o recurso
 
 def solicitar_recurso(client_id):
+    global flag
+    global client
     if not flag:
         flag = True
         client = client_id
@@ -12,13 +14,19 @@ def solicitar_recurso(client_id):
         return False
 
 def liberar_recurso(client_id):
+    global flag
+    global client
     if flag and client == client_id:
         flag = False
-        client_id = None
+        client = None
         return True
     else:
         return False
 
 server = SimpleXMLRPCServer(("192.168.0.115", 8000))
+print("Servidor XML-RPC em execução...")
+
+server.register_function(solicitar_recurso, "solicitar_recurso")
+server.register_function(liberar_recurso, "liberar_recurso")
 
 server.serve_forever()
