f2ef7547fc92640cf03234ef4d515f6a00b63797
ArimaBatista <80778627+ArimaBatista@users.noreply.github.com>
2024-06-30 19:31:09 -0300

    Create ldclient.py

---
+++ ldclient.py
@@ -0,0 +1,19 @@
+from xmlrpc.client import ServerProxy
+import socket
+
+# Obter o endereço IP do host local
+hostname = socket.gethostname()
+endereco_ip = socket.gethostbyname(hostname)
+
+# Lista de endereços do servidor
+end_serve = ["http://192.168.0.115:8000", "http://192.168.0.116:8000", "http://192.168.0.118:8000"]
+
+while True:
+    for x in end_serve:
+        try:
+            serve = ServerProxy(x)
+            serve.registro(endereco_ip)
+            dado = serve.lider()
+            print(f"Líder do servidor {x} é {dado}")
+        except Exception as e:
+            print(f"Servidor {x} está desativado. Erro: {e}")
