1ddec172e2922eb0f208be57fadc7bca11c938d1
ArimaBatista <80778627+ArimaBatista@users.noreply.github.com>
2024-06-30 17:02:12 -0300

    Update exclient.py

--- exclient.py
+++ exclient.py
@@ -3,30 +3,31 @@ import uuid
 
 client_id = str(uuid.uuid1())
 
-def solicitar_recurso(serve):
+end_serve = "http://192.168.0.115:8000"
+
+serve = ServerProxy(end_serve)
+
+def solicitar_recurso():
         # Tenta pegar o recurso
-        resultado = server.solicitar_recurso(client_id)
+        resultado = serve.solicitar_recurso(client_id)
         if resultado:
             print(f"Cliente {client_id} obteve o recurso.")
         else:
             print(f"Cliente {client_id} não obteve o recurso. Recurso em uso.")
         return resultado
 
-def liberar_recurso(self):
+def liberar_recurso():
         # Tenta liberar o recurso
-        resultado = server.liberar_recurso(client_id)
+        resultado = serve.liberar_recurso(client_id)
         if resultado:
             print(f"Cliente {client_id} liberou o recurso.")
         else:
             print(f"Cliente {client_id} não pôde liberar o recurso. Possível erro.")
 
 
-end_serve = "http://192.168.0.115:8000"
-
-server = ServerProxy(end_serve)
 
 while True:
     input("Pegar Recurso")
-    solicitar_recurso(serve)
+    solicitar_recurso()
     input("Liberar Recurso")
-    liberar_recurso(serve)
+    liberar_recurso()
