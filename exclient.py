156a73c7825e0a51b1cd00f362495201613e60f1
ArimaBatista <80778627+ArimaBatista@users.noreply.github.com>
2024-06-30 16:21:45 -0300

    Update exclient.py

--- exclient.py
+++ exclient.py
@@ -1,14 +1,14 @@
 from xmlrpc.client import ServerProxy
 import uuid
 
-client_id = uuid.uuid1()
+client_id = str(uuid.uuid1())
 
 class ExclusaoMutuaCliente:
     def __init__(self, server_address):
         self.server = ServerProxy(server_address)
 
     def solicitar_recurso(self):
-        #tenta pega o rec
+        # Tenta pegar o recurso
         resultado = self.server.solicitar_recurso(client_id)
         if resultado:
             print(f"Cliente {client_id} obteve o recurso.")
@@ -17,19 +17,16 @@ class ExclusaoMutuaCliente:
         return resultado
 
     def liberar_recurso(self):
-        #tenta libera o rec
+        # Tenta liberar o recurso
         resultado = self.server.liberar_recurso(client_id)
         if resultado:
             print(f"Cliente {client_id} liberou o recurso.")
         else:
             print(f"Cliente {client_id} não pôde liberar o recurso. Possível erro.")
 
-
-
-
 cliente = ExclusaoMutuaCliente("http://192.168.0.115:8000")
-while (0==0):
-    t = input("Pegar Recurso")
+while True:
+    input("Pegar Recurso")
     cliente.solicitar_recurso()
-    t = input("Liberar Recurso")
+    input("Liberar Recurso")
     cliente.liberar_recurso()
