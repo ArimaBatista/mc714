e941b5f4b719ffae2245a15c33836ec1f7f96d3d
ArimaBatista <80778627+ArimaBatista@users.noreply.github.com>
2024-06-30 13:31:27 -0300

    Create exserver.py

---
+++ exserver.py
@@ -0,0 +1,35 @@
+from xmlrpc.server import SimpleXMLRPCServer
+
+class ExclusaoMutuaServer:
+    def __init__(self):
+        self.flag = False  # Indica se o recurso está em uso
+        self.client_id = None  # ID do cliente que possui o recurso
+
+    def solicitar_recurso(self, client_id):
+        """
+        Método para solicitar acesso ao recurso.
+        Retorna True se o recurso estiver disponível, False caso contrário.
+        """
+        if not self.flag:
+            self.flag = True
+            self.client_id = client_id
+            return True
+        else:
+            return False
+
+    def liberar_recurso(self, client_id):
+        """
+        Método para liberar o recurso.
+        Retorna True se o cliente possuía o recurso e o liberou com sucesso, False caso contrário.
+        """
+        if self.flag and self.client_id == client_id:
+            self.flag = False
+            self.client_id = None
+            return True
+        else:
+            return False
+
+server = SimpleXMLRPCServer(("localhost", 8000))
+server.register_instance(ExclusaoMutuaServer())
+print("Servidor de Exclusão Mútua em execução na porta 8000")
+server.serve_forever()
