a42a5ebe1681abecb01430c982ad28397e7e7bfe
ArimaBatista <80778627+ArimaBatista@users.noreply.github.com>
2024-06-30 19:12:57 -0300

    Create ldserver.py

---
+++ ldserver.py
@@ -0,0 +1,71 @@
+from xmlrpc.server import SimpleXMLRPCServer
+from xmlrpc.client import ServerProxy
+import socket
+import re
+
+# Obter o endereço IP do host local
+hostname = socket.gethostname()
+endereco_ip = socket.gethostbyname(hostname)
+
+# Lista de clientes e líder
+client = []
+lider = None
+
+def ativo():
+    return "ativo"
+
+def registro(client_id):
+    global client
+    if client_id not in client:
+        client.append(client_id)
+        return "adicionado"
+    else:
+        return "ja presente"
+
+def obter_lider():
+    global lider
+    try:
+        p = f"http://{lider}:8080"
+        serve = ServerProxy(p)
+        verifica = serve.ativo()
+        if verifica == "ativo":
+            return lider
+    except:
+        eleicao()
+    return lider
+
+def eleicao():
+    global client
+    global endereco_ip
+    global lider
+    id = re.sub(r'[^0-9]', '', endereco_ip)
+    id = int(id)
+    lista = sorted(client)
+    lider = endereco_ip
+    for x in lista:
+        y = re.sub(r'[^0-9]', '', x)
+        y = int(y)
+        if y > id:
+            try:
+                p = f"http://{x}:8080"
+                serve = ServerProxy(p)
+                verifica = serve.ativo()
+                if verifica == "ativo":
+                    lider = x
+                    break
+            except:
+                client.remove(x)
+    return lider
+
+# Criar o servidor XML-RPC
+server = SimpleXMLRPCServer((endereco_ip, 8000))
+print(f"Servidor em execução no endereço {endereco_ip}:8000")
+
+# Registrar funções no servidor
+server.register_function(ativo, "ativo")
+server.register_function(registro, "registro")
+server.register_function(obter_lider, "lider")
+server.register_function(eleicao, "eleicao")
+
+# Iniciar o servidor
+server.serve_forever()
