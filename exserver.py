d54f3d54edc2039175e3661f82dcdc296fcc7d6b
ArimaBatista <80778627+ArimaBatista@users.noreply.github.com>
2024-06-30 16:58:44 -0300

    Update exserver.py

--- exserver.py
+++ exserver.py
@@ -1,25 +1,24 @@
 from xmlrpc.server import SimpleXMLRPCServer
 
-def solicitar_recurso(self, client_id):
-    if not self.flag:
-        self.flag = True
-        self.client_id = client_id
+flag = False  # Indica se o recurso está em uso
+client = None  # ID do cliente que possui o recurso
+
+def solicitar_recurso(client_id):
+    if not flag:
+        flag = True
+        client = client_id
         return True
     else:
         return False
 
-def liberar_recurso(self, client_id):
-    if self.flag and self.client_id == client_id:
-        self.flag = False
-        self.client_id = None
+def liberar_recurso(client_id):
+    if flag and client == client_id:
+        flag = False
+        client_id = None
         return True
     else:
         return False
 
 server = SimpleXMLRPCServer(("192.168.0.115", 8000))
-
-
-self.flag = False  # Indica se o recurso está em uso
-self.client_id = None  # ID do cliente que possui o recurso
 
 server.serve_forever()
